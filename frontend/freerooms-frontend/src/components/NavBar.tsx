a34952ed4d48507662a812c3650e2520989b04c8
Chloe Yeung <yeung.wing.chloe@gmail.com>
2025-02-27 08:54:15 +1100

    solved fe stretch task 1

--- frontend/freerooms-frontend/src/components/NavBar.tsx
+++ frontend/freerooms-frontend/src/components/NavBar.tsx
@@ -1,10 +1,21 @@
 import { Search, Map, GridView, DarkMode } from '@mui/icons-material';
+import { useState } from 'react';
 
 export default function NavBar() {
+  const [isDoorOpen, setIsDoorOpen] = useState(true);
+
+  const toggleDoor = () => {
+    setIsDoorOpen((prev_bool) => !prev_bool);
+  }
+
   return (
     <nav className='flex items-start justify-between border-b-1 border-gray-300 px-4'>
-      <div className='flex gap-0.5 py-1'>
-        <img src='/assets/freeRoomsLogo.png' alt='Freerooms Logo' className='h-12.5' />
+      <div className='flex gap-0.5 py-1 cursor-pointer' onClick={toggleDoor}>
+        <img
+          src={isDoorOpen ? '/assets/freeRoomsLogo.png' : '/assets/freeroomsDoorClosed.png'}
+          alt='Freerooms Logo'
+          className='h-12.5'
+        />
         <span className='font-semibold text-[2rem] text-orange-500'>Freerooms</span>
       </div>
 
